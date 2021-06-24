693213de8e4ae53d20231d34b6bea6fce916f2ba
avimistry4800 <avimistry4800@google.com>
2021-06-25 00:30:20 +0600

    refine

--- index.js
+++ index.js
@@ -7,59 +7,69 @@ const ObjectID = require('mongodb').ObjectId;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
- 
+
 const port = 5000;
- 
- 
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(fileUpload());
- 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pbmsk.mongodb.net/${process.env.DB_USER}?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pbmsk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect((err) => {
-  console.log(err);
-  const blogCollection = client.db("redium").collection("blogs");
+    console.log(err);
+    const blogCollection = client.db("redium").collection("blog");
 
- 
-  // Add data to database 
- 
-  app.post('/addblog', (req, res) => {
-    const file = req.files.image;
-    const title = req.body.title;
-    const content = req.body.content;
-    const filePath = `${__dirname}/blogs/${file.title}`;
-    const newImage = file.data;
-    const convertImage = newImage.toString("base64");
-    const image = {
-      contentType: file.mimetype,
-      size: file.size,
-      img: Buffer.from(convertImage, "base64"),
-    };
-    blogCollection.insertOne({ title, content, image })
-      .then((result) => {
-        res.send(result.insertedCount > 0);
-        console.log("Blog add to database")
-      });
- 
-  });
- 
- 
+
+    // Add data to database 
+
+    app.post('/addblog', (req, res) => {
+        const file = req.files.image;
+        const title = req.body.title;
+        const content = req.body.content;
+        const filePath = `${__dirname}/blogs/${file.title}`;
+        const newImage = file.data;
+        const convertImage = newImage.toString("base64");
+        console.log(file)
+        const image = {
+            contentType: file.mimetype,
+            name: file.name,
+            size: file.size,
+            img: Buffer.from(convertImage, "base64"),
+        };
+        blogCollection.insertOne({ title, content, image })
+            .then((result) => {
+                res.send(result.insertedCount > 0);
+                console.log("Blog add to database")
+            });
+
+    });
+
+
+    //load blog 
+    app.get("/blogs", (req, res) => {
+        blogCollection.find().toArray((err, blog) => {
+            res.send(blog)
+        });
+    });
+
+
 });
- 
- 
- 
- 
- 
+
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello')
+    res.send('Hello')
 })
- 
- 
- 
- 
- 
+
+
+
+
+
 app.listen(process.env.PORT || port)
